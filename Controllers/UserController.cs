2ad98ea45b61465d24c7406503b50bc32ff04461
haiderinam7 <157199010+haiderinam7@users.noreply.github.com>
2024-01-27 17:11:12 +1100

    Create a delete endpoint

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -99,6 +99,21 @@ public class UserController : ControllerBase
         throw new Exception("Failed to Add User");
 
     }
+    [HttpDelete("DeleteUser/{userId}")]
+
+    public IActionResult DeleteUser(int userId)
+    {    string sql = @"
+    DELETE FROM TutorialAppSchema.Users
+    WHERE UserId = " + userId.ToString();
+
+        if (_dapper.ExecuteSql(sql))
+        {
+            return Ok();
+        }
+
+        throw new Exception("Failed to Delete User");
+
+    }
 
     
 }
